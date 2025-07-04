78eddd70db8290374f9195e4d5791e7dc469c851
Palurusahithi <palurusahithi2004@gmail.com>
2025-07-04 18:00:52 +0530

    Java Q2

---
+++ Question_2.java
@@ -0,0 +1,14 @@
+package javafundamentals;
+
+public class Question_2 {
+
+	public static void main(String[] args) {
+		if(args.length<1) {
+			System.out.println("enter 1 argument");
+		}
+		String sample=args[0];
+		System.out.println(" Welcome " + sample);
+
+	}
+
+}
