92d724211d44f19687efbeaa29505f25ed5f8b40
Palurusahithi <palurusahithi2004@gmail.com>
2025-07-04 18:36:16 +0530

    Java Q4

---
+++ Question_4.java
@@ -0,0 +1,25 @@
+package javafundamentals;
+
+import java.util.Scanner;
+
+public class Question_4 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num=s.nextInt();
+		
+
+	
+	if(num>0) {
+		System.out.println("positive");
+	}
+	else if(num<0) {
+		System.out.println("negative");
+	}
+	else {
+		System.out.println("zero");
+	}
+
+}
+}
