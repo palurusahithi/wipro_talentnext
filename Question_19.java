a21267a24d02b27f7c7a7398020cb580dc688e3e
Palurusahithi <palurusahithi2004@gmail.com>
2025-07-05 10:17:50 +0530

    Java Q19

---
+++ Question_19.java
@@ -0,0 +1,19 @@
+package javafundamentals;
+
+public class Question_19 {
+
+		
+
+			public static void main(String args[]){
+				int n = 3;
+				for (int i=1;i<=n;i++) {
+					for (int j=1;j<=i;j++) {
+						System.out.print("*");
+					}
+					System.out.println(" ");
+				}
+				
+				 
+			}
+
+		}
